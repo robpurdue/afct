8be9276525959c863600ec8bae9ea9f9aecc16ff
Rob Elliott <ree@purdue.edu>
2024-10-29 17:09:11 -0400

    Typo in setup_shell.sh

--- setup_shell.sh
+++ setup_shell.sh
@@ -20,7 +20,7 @@ echo -e "- Remember to use the CLASSIC version of Azure Cloud Shell! Go to Setti
 echo -e "- To start your Python virtual environment, type the command \e[1;34mstart_venv\e[0m"
 echo -e "- To install Azure Cloud Function Tools, type the command \e[1;34minstall_afct\e[0m"
 echo -e "==="
-echo =e ""
+echo -e ""
 
 EOT
 
