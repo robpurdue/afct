7b0d76a16e4750d449aee7488fbca1e53673cd5c
Rob Elliott <ree@purdue.edu>
2024-10-30 06:44:50 -0400

    Typo in setup_shell

--- setup_shell.sh
+++ setup_shell.sh
@@ -16,16 +16,16 @@ if ! grep -Fxq "$line_to_check" ~/.bashrc; then
     # Create the formatted lines
     cat << 'EOT' >> ~/.bashrc
 
-    # Azure Cloud Shell Information for MGMT 59000
-    echo -e ""
-    echo -e "=== NOTES FOR MGMT 59000 CLOUD COMPUTING ==="
-    echo -e "- Remember to use the CLASSIC version of Azure Cloud Shell! Go to Settings > 'Go to Classic version'"
-    echo -e "- To start your Python virtual environment, type the command \e[1;34mstart_venv\e[0m"
-    echo -e "- To install Azure Cloud Function Tools, type the command \e[1;34minstall_afct\e[0m"
-    echo -e "==="
-    echo -e ""
+# Azure Cloud Shell Information for MGMT 59000
+echo -e ""
+echo -e "=== NOTES FOR MGMT 59000 CLOUD COMPUTING ==="
+echo -e "- Remember to use the CLASSIC version of Azure Cloud Shell! Go to Settings > 'Go to Classic version'"
+echo -e "- To start your Python virtual environment, type the command \e[1;34mstart_venv\e[0m"
+echo -e "- To install Azure Cloud Function Tools, type the command \e[1;34minstall_afct\e[0m"
+echo -e "==="
+echo -e ""
 
-    EOT
+EOT
 
     # Update the .bashrc file to add the command to install AFCT
     echo "alias install_afct='~/clouddrive/afct/install_afct.sh'" >> ~/.bashrc
